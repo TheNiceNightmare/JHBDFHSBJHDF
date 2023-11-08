60c76e1b02eee9a4564cfe9a28c64a54cc9c6b5a
Javier Jara <javier.jara@tecnologicoleciocristiano.edu.ec>
2023-11-07 21:13:05 -0500

    Validaciones

--- JHBDFHSBJHDF/MainPage.xaml.cs
+++ JHBDFHSBJHDF/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+        private string titulo = "Registro";
         public Estudiante MiEstudiante { get; set; }
         public MainPage()
         {
@@ -11,7 +12,30 @@
         }
         private void Confirmar_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Registro", MiEstudiante.Nombre + " sus datos se ingresaron correctamente.", "Ok");
+            if (IsValid())
+            {
+                DisplayAlert("Registro", MiEstudiante.Nombre + " sus datos se ingresaron correctamente.", "Ok");
+            }
+        }
+        private bool IsValid()
+        {
+            if (String.IsNullOrEmpty(MiEstudiante.Nombre))
+            {
+                DisplayAlert(titulo, "Debe ingresar un nombre", "ok");
+                return false;
+            }
+            
+            if (String.IsNullOrEmpty(MiEstudiante.Apellido))
+            {
+                DisplayAlert(titulo, "Debe ingresar un Apellido", "ok");
+                return false;
+            }
+            if (String.IsNullOrEmpty(MiEstudiante.Materia))
+            {
+                DisplayAlert(titulo, "Debe ingresar su materia", "ok");
+                return false;
+            }
+            return true;
         }
     }
 }
